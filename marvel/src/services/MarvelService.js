d69b9f838034a914ccc48fdf1cf035a27ab0503c
Vakulio <vlad.vakula@gmail.com>
2022-03-15 22:19:54 +0300

    add searching bar

--- marvel/src/services/MarvelService.js
+++ marvel/src/services/MarvelService.js
@@ -19,6 +19,11 @@ const useMarvelService = () => {
         return _tranformCharacter(res.data.results[0])
     }
 
+    const getCharacterByName = async (name) => {
+        const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+        return res.data.results.map(_tranformCharacter);
+    }
+
     const getAllComics = async (offset = 0) => {
         const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_tranformComics);
@@ -53,7 +58,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic}
+    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic, getCharacterByName}
 }
 
 export default useMarvelService;
